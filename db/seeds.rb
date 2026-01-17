8bedd9f86ff4f77a43182f8289cbeae1a12515a7
Troy Zangara <troyzangara@gmail.com>
2026-01-17 15:44:24 +0900

    Implemented routes, controllers, and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "json"
+require "open-uri"
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating movies from TMDB API..."
+
+# The URL provided by the Le Wagon proxy
+url = "https://tmdb.lewagon.com/movie/top_rated"
+user_serialized = URI.open(url).read
+movies = JSON.parse(user_serialized)["results"]
+
+movies.each do |movie|
+  puts "Creating #{movie['title']}"
+  # base URL for images is https://image.tmdb.org/t/p/w500/, https://developer.themoviedb.org/docs/image-basics
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Finished! Created #{Movie.count} movies."
